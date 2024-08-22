4ad8ac313831d5f5add6ea4dfaded1058764665d
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-08-22 10:00:15 +0545

    js type conversion

---
+++ index.js
@@ -0,0 +1,33 @@
+//variable declaration
+//camelCase
+
+//const -> keyword
+// keyword variableName operator value
+
+const college = "Kingsway";
+// college = "Kingsway College";
+// const college = "Kingsway COllege";
+const phoneNumber = 42323123;
+const isCollege = false;
+
+//let
+let nameOfStudent = "John";
+//re assign
+nameOfStudent = "John Doe";
+// let nameOfStudent = "Krishna";
+console.log("Hello", nameOfStudent)
+
+let age = 23;
+let isStudent = true;
+//var
+var city = "Mumbai";
+//reassign
+city = "Pune";
+//redeclare
+var city = "Kathmandu";
+
+console.log(city)
+
+var countryCode = 91;
+var isCity = true;
+
---
+++ types.js
@@ -0,0 +1,20 @@
+const number1 = 10;
+const number2 = 20;
+const sum = number1 * number2;
+
+const string1 = "Krishna";
+const string2 = "Radha";
+const combined = string1 + string2;
+console.log(combined)
+
+//combined cases
+const a1 = 10;
+const a2 = 20;
+const a3 = "apple";
+const a4 = 40;
+const isFemale = true;
+
+const s1 = String(a1) + isFemale;
+
+console.log(s1)
+
