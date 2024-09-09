9d260ed0e1987f3aeb6ad5122d7d3f928bb1c0a2
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-09-09 09:55:43 +0545

    dom

---
+++ dom.js
@@ -0,0 +1,50 @@
+// window.console.log("Hello")
+// window.document
+
+// h2{
+// }
+// .hello{}
+// #hellp{}
+
+// const element = document.getElementsByClassName("name-class");
+
+// element.style.color = "red";
+
+// console.log(element)
+// pratik.innerText = "Pratik Gaire is a CR";
+
+// const greet = document.getElementById("greetings")
+
+// greet.innerText = "Hello, Pratik";
+
+// console.log(element)
+
+// const elementByClass = document.getElementsByClassName("name-class");
+
+// for (let index = 0; index <= elementByClass.length - 1; index++) {
+//   elementByClass[index].style.color = "red";
+// }
+
+// console.log(elementByClass)
+
+// const elementByTag = document.getElementsByTagName("h2");
+
+// for (let index = 0; index <= elementByTag.length - 1; index++) {
+//   console.log((elementByTag[index].style.color = "blue"));
+// }
+
+// console.log(elementByTag);
+
+function changeColorToRed() {
+  const element = document.getElementById("name");
+
+  element.style.color = "red";
+}
+
+function changeColorToBlue() {
+  const element = document.getElementById("name");
+
+  element.style.color = "blue";
+}
+
+
