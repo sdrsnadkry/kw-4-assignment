073292105eb3c4624736c2a4d58675cb916c17bd
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-09-13 09:53:47 +0545

    form submit

--- form.js
+++ form.js
@@ -1,21 +1,70 @@
 function submitValue() {
   const input = document.getElementById("name-input");
-  const contactName = input.value
-  
-  const nameValue = document.getElementById("name-value")
-  nameValue.innerText = contactName
+  const contactName = input.value;
 
-  const popup  = document.getElementById("contact-popup")
-  
-  popup.setAttribute("class", "bg-[#00000050] absolute left-0 right-0 top-0 bottom-0")
-  
-  console.log(popup.classList)
+  const nameValue = document.getElementById("name-value");
+  nameValue.innerText = contactName;
+
+  const popup = document.getElementById("contact-popup");
+
+  popup.setAttribute(
+    "class",
+    "bg-[#00000050] fixed left-0 top-0 w-full h-full"
+  );
+
+  console.log(popup.classList);
+}
+
+function closePopUP() {
+  const popup = document.getElementById("contact-popup");
+  popup.setAttribute(
+    "class",
+    "bg-[#00000050] fixed left-0 top-0 w-full h-full hidden"
+  );
+}
+
+function stopA(event) {
+  event.preventDefault();
 }
 
 
-function closePopUP(){
-  const popup  = document.getElementById("contact-popup")
-  popup.setAttribute("class", "bg-[#00000050] absolute left-0 right-0 top-0 bottom-0 hidden")
 
+function submitForm(event) {
+  event.preventDefault();
+
+  const form = document.forms[0];
+
+  const email = form.email.value;
+
+  const password = form.password.value;
+
+  const cPassword = form["confirm-password"].value;
+
+  if(cPassword !== password){
+    alert("Password does not match")
+  }
+
+  console.log(email, password, cPassword);
+}
+
+
+function viewPassword(){
+  const passwordInput = document.getElementById("pwd-input")
+
+  passwordInput.setAttribute("type", "text")
+
+  const spanTag = document.getElementById("eye-icon-span");
+  spanTag.innerHTML = "<box-icon type='solid' name='bullseye'></box-icon>"
+
+}
+
+
+function hidePassword(){
+  const passwordInput = document.getElementById("pwd-input")
+
+  passwordInput.setAttribute("type", "password")
+
+   const spanTag = document.getElementById("eye-icon-span");
+  spanTag.innerHTML = "<box-icon name='low-vision'></box-icon>"
 
 }
